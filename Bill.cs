138e4995d8a623115e798bd7b41fa3d6f115460c
luongse <61089522+luongse@users.noreply.github.com>
2021-03-17 02:06:56 +0700

    update class Bill

---
+++ Bill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTea
+{
+    class Bill
+    {
+        string BillID { get; set; }
+        string TimeOfCreate { get; set; }
+        float SumOfMoney { get; set; }
+    }
+}
